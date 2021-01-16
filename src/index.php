5d28d387ba19a6bbccd7699291f6496c151e6362
Max Base <MaxBaseCode@Gmail.Com>
2021-01-17 01:01:33 +0330

    Improve webserice: count view of post/remove comments/cache rates

--- src/index.php
+++ src/index.php
@@ -1,41 +1,44 @@
 <?php
+// Max Base
 // https://github.com/BaseMax/API-Sarrafchi
+
 header("Access-Control-Allow-Origin: *");
+
 require 'flight/Flight.php';
 include "bonbast-api/source/bonbast-api.php";
-require "phpedb.php";
 
-date_default_timezone_set('Asia/Tehran');
-ini_set('display_startup_errors', 1);
-ini_set('display_errors', 1);
-error_reporting(-1);
+require "_core.php";
+
 Flight::set('flight.log_errors', true);
 
-$db=new database();
-$db->db="db_sarrafchi";
-$db->connect("localhost", "root", "dfgdfgdfgdfgfgfghfgh");
-
-// $backup=[];
 Flight::route('/', function(){
    global $db;
+
    echo 'hello world!';
-   $posts=$db->selects("news");
-   // foreach($posts as $i=>$post) {
-   //    // $db->update("news", ["id"=>$post["id"]], ["date"=>trim($post["date"])]);
-   //    $date=$post["date"];
-   //    $backup[]=$date;
-   //    $split=explode(" - ", $date);
-   //    $date=$split[0];
-   //    $time=$split[1];
-   //    $db->update("news", ["id"=>$post["id"]], ["date"=>$date, "time"=>$time]);
-   //    // $db->update("news", ["id"=>$post["id"]], ["slug"=>urldecode($post["slug"])]);
-   // }
-   // print json_encode($backup);
 });
 
+Flight::route('/log', function() {
+    	date_default_timezone_set('Asia/Tehran');
+	$car=$_GET["car"];
+	$latitude=$_GET["latitude"];
+	$longitude=$_GET["longitude"];
+	file_put_contents("log.txt", date("Y/m/d h:i:s"). " => ". $car.",".$latitude.",".$longitude."\n", FILE_APPEND);
+	Flight::json([]);
+});
 Flight::route('/rate', function(){
    global $db;
-   $prices=bonbast();
+
+   // $prices=bonbast();
+   $data = $db->select("history", [], "ORDER BY `id` DESC LIMIT 1", "data");
+   if(!isset($data["data"])) { $data=["data"=>"{}"]; }
+   $prices = json_decode($data["data"], true);
+   if($prices == [] || $prices == null) {
+      Flight::json([
+         "status"=>"failed",
+         "currencies"=>[],
+      ]);
+      return;
+   }
    Flight::json([
       "status"=>"success",
       "currencies"=>$prices,
@@ -44,6 +47,7 @@ Flight::route('/rate', function(){
 
 Flight::route('/blog', function(){
    global $db;
+
    $page=1;
    $post_per_page=20;
    if(isset($_GET["page"])) {
@@ -61,7 +65,6 @@ Flight::route('/blog', function(){
    else {
       foreach($posts as $i=>$post) {
          $posts[$i]["excerpt"]=$posts[$i]["subtext"] ." ". $posts[$i]["text"];
-         // print $posts[$i]["excerpt"]."\n";
          $posts[$i]["excerpt"]=strWordCut($posts[$i]["excerpt"], 240);
          unset($posts[$i]["text"]);
          unset($posts[$i]["subtext"]);
@@ -73,9 +76,9 @@ Flight::route('/blog', function(){
    }
 });
 
-// Flight::route('/blog/@slug-@id', function($slug, $id){
 Flight::route('/blog/@slug', function($slug){
    global $db;
+
    $post=$db->select("news", ["slug"=>$slug]);
    if($post == null || $post == []) {
       Flight::json([
@@ -83,6 +86,7 @@ Flight::route('/blog/@slug', function($slug){
       ]);
    }
    else {
+      $db->update("news", ["id"=>$post["id"]], ["view"=>$post["view"]+1]);
       Flight::json([
          "status"=>"success",
          "post"=>$post,
@@ -91,13 +95,3 @@ Flight::route('/blog/@slug', function($slug){
 });
 
 Flight::start();
-
-function strWordCut($string, $length) {
-   $str_len = mb_strlen($string);
-   $string = strip_tags($string);
-   if($str_len > $length) {
-      $stringCut = mb_substr($string, 0, $length-15);
-      $string = $stringCut.'.....' . mb_substr($string, $str_len-10, $str_len-1);
-   }
-   return $string;
-}
